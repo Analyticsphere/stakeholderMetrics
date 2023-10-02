465fe63b7caf252d589de415c35e4f142d0c9219
Rebecca Sansale <32822979+rebexxxxxx@users.noreply.github.com>
2023-10-02 14:33:45 -0400

    Add files via upload

--- activities_plot.R
+++ activities_plot.R
@@ -3,13 +3,19 @@
 activities_plot<- function(){
 options(knitr.table.format = "latex")
 currentDate <- Sys.Date()-2
+library(expss)
 
-
+#defining the variables that will be pulled from GCP
 modules <- c("d_100767870","d_949302066","d_536735468","d_663265240","d_976570371","d_517311251","d_832139544","d_264644252","d_770257102")
-bio.col <- c("d_684635302","d_878865966", "d_167958071", "d_173836415_d_266600170_d_915179629", "d_173836415_d_266600170_d_718172863", "d_173836415_d_266600170_d_592099155", "d_173836415_d_266600170_d_561681068", "d_173836415_d_266600170_d_847159717", "d_173836415_d_266600170_d_448660695", "d_173836415_d_266600170_d_139245758", "d_173836415_d_266600170_d_541311218", "d_173836415_d_266600170_d_224596428", "d_173836415_d_266600170_d_740582332", "d_173836415_d_266600170_d_982213346", "d_173836415_d_266600170_d_398645039", "d_173836415_d_266600170_d_822274939")
-###clinical collection time:
-clc.bldtm <- c("d_173836415_d_266600170_d_769615780","d_173836415_d_266600170_d_822274939","d_173836415_d_266600170_d_398645039","d_173836415_d_266600170_d_982213346","d_173836415_d_266600170_d_740582332")
-clc.urinetm <- c("d_173836415_d_266600170_d_139245758","d_173836415_d_266600170_d_224596428","d_173836415_d_266600170_d_541311218","d_173836415_d_266600170_d_939818935","d_173836415_d_266600170_d_740582332")
+bio.col <- c("d_684635302","d_878865966", "d_167958071", "d_173836415_d_266600170_d_915179629", "d_173836415_d_266600170_d_718172863",
+             "d_173836415_d_266600170_d_592099155", "d_173836415_d_266600170_d_561681068", "d_173836415_d_266600170_d_847159717", 
+             "d_173836415_d_266600170_d_448660695", "d_173836415_d_266600170_d_139245758", "d_173836415_d_266600170_d_541311218", 
+             "d_173836415_d_266600170_d_224596428", "d_173836415_d_266600170_d_740582332", "d_173836415_d_266600170_d_982213346", 
+             "d_173836415_d_266600170_d_398645039", "d_173836415_d_266600170_d_822274939")
+clc.bldtm <- c("d_173836415_d_266600170_d_769615780","d_173836415_d_266600170_d_822274939","d_173836415_d_266600170_d_398645039",
+               "d_173836415_d_266600170_d_982213346","d_173836415_d_266600170_d_740582332")
+clc.urinetm <- c("d_173836415_d_266600170_d_139245758","d_173836415_d_266600170_d_224596428","d_173836415_d_266600170_d_541311218",
+                 "d_173836415_d_266600170_d_939818935","d_173836415_d_266600170_d_740582332")
 var.list <- c("token","Connect_ID","d_821247024","d_914594314","d_512820379","state_d_158291096","d_471593703","d_827220437",
               "d_130371375_d_266600170_d_787567527","d_130371375_d_266600170_d_731498909",bio.col,modules)
 
@@ -37,6 +43,8 @@ for (i in 1: length(cnames)){
   data[,cnames[i]] <- ifelse(numbers_only(var), as.numeric(as.character(var)), var) #converting variables to numeric if applicable
 }
 
+#first round of filtering
+#keeping participants who are not actively being recruited
 #d_512820379 = recruitment type, where 180583933 == not active
 #d_821247024 = match verification where 197316935 = cannot be verified
 data <- data[which(data$d_512820379 != 180583933 | data$d_821247024 == 197316935),] #0 removeal
@@ -50,6 +58,9 @@ data <- data[which (data$d_821247024 != 922622075 | data$d_512820379 !=486306141
 
 #defining total recruitment, active/passive recruitment
 #defining verified participants
+#in order to calculate the number of participants by activity,
+#we have to count the cumulative verified participants, then
+#separate by activity. 
 ########################################################################################################################################################
 ########################################################################################################################################################
 ########################################################################################################################################################
@@ -64,7 +75,7 @@ veri_resp <- data[which(data$d_512820379 != 486306141 | data$d_821247024 != 9226
          elgible.time = ymd_hms(d_130371375_d_266600170_d_787567527))
 veri_resp$recrstart.date <-  as_date(min(ymd_hms(veri_resp$d_471593703),na.rm=TRUE))
 
-
+#naming sites
 veri_resp <- apply_labels(veri_resp,d_827220437 = "Site",#RcrtES_Site_v1r0
                           d_827220437 = c("HealthPartners"= 531629870, "Henry Ford Health System"=548392715, 
                                           "Kaiser Permanente Colorado" = 125001209, "Kaiser Permanente Georgia" = 327912200,
@@ -78,31 +89,24 @@ veri_resp$site <- factor(veri_resp$d_827220437,
                                   "Kaiser Permanente Georgia","Kaiser Permanente Hawaii","Kaiser Permanente Northwest",
                                   "National Cancer Institute","Other"))
 veri_resp$site <- droplevels(veri_resp$site)
+#recruitment start date, creating a variable that starts at the beginning of the recruitment
+#where the first week of recruitment for the entire CC study is 1
 recrstart.date <- as_date(min(ymd_hms(veri_resp$recru_time),na.rm=TRUE))
-#wday(unique(veri_resp$recrstart.date), week_start = 1)
 as_date(recrstart.date-days(5))
-#[1] "2021-07-18" #Sunday the censoring starting day
-#[1] 1 #Sunday
+#"2021-07-18" #Sunday the censoring starting day
+#1 #Sunday
 #calculating when recruitment of an individual started and when they were recruited
 veri_resp$recruit.week <- ceiling(as.numeric(difftime(as_date(veri_resp$recru_time), as_date(veri_resp$recrstart.date-days(5)),units="days"))/7) #to lock at Monday as the start of week
 veri_resp$recruit.week.date <- veri_resp$recrstart.date + days(2) + dweeks(veri_resp$recruit.week-1) #to present the date of the starting date in the following week
 summary(as_date(veri_resp$recruit.week.date))
-# Min.      1st Qu.       Median         Mean      3rd Qu.         Max. 
-# "2021-07-25" "2022-08-14" "2022-11-06" "2022-10-25" "2023-01-15" "2023-03-26" 
 
 #time between an individual being recruited and being verified
 veri_resp$verified.week <- ceiling(as.numeric(difftime(as_date(ymd_hms(veri_resp$d_914594314)),as_date(veri_resp$recrstart.date-days(5)),units="days"))/7)
 veri_resp$verified.week.date <- (veri_resp$recrstart.date) + days(2)+ dweeks(veri_resp$verified.week-1)
 
-min(as_date(ymd_hms(veri_resp$verified_time)),na.rm=TRUE)
-# [1] "2021-07-27"
-unique(as_date(min(veri_resp$verified.week.date,na.rm=TRUE)))
-#[1] "2021-08-01"
-unique(as_date(max(veri_resp$verified.week.date,na.rm=TRUE)))
-#[1] "2023-03-26"
 
 
-###to create the time censoring variables by week;
+#create the time censoring variables by week
 veri_resp <- veri_resp[which(veri_resp$recruit.week.date < currentDate),]
 #as Michelle and Amelia want to set up the censor week as Monday to Sunday (the Sunday would be the last day of the week, and the report is done every Monday),
 #the data on the week of the checking day would be excluded from the analytic dataset 
@@ -156,16 +160,6 @@ recrui_wk_verified$total_recruits.cum <- cumsum(recrui_wk_verified$active_recrui
 #cumulative total verified = cumulative sum(total_verified)
 recrui_wk_verified$total_verified.cum <- cumsum(recrui_wk_verified$total_verifieds)
 
-#new response rate:
-recrui_wk_verified$new.active.response.rate <- ifelse(recrui_wk_verified$active_recruits >0, 100*(recrui_wk_verified$active_verifieds/recrui_wk_verified$active_recruits), 0)
-recrui_wk_verified$new.passive.response.rate <- ifelse(recrui_wk_verified$active_recruits!=0, 100*(recrui_wk_verified$passive_verifieds/recrui_wk_verified$active_recruits),NA)
-recrui_wk_verified$new.total.response.rate <- ifelse(recrui_wk_verified$active_recruits>0, 100*(recrui_wk_verified$total_verifieds/recrui_wk_verified$active_recruits), 0)
-
-#cumalative rate
-recrui_wk_verified$cum.active.response.rate <- 100*(cumsum(recrui_wk_verified$active_verifieds)/cumsum(recrui_wk_verified$active_recruits))
-recrui_wk_verified$cum.passive.response.rate <- 100*(cumsum(recrui_wk_verified$passive_verifieds)/cumsum(recrui_wk_verified$active_recruits))
-recrui_wk_verified$cum.total.response.rate <- 100*(cumsum(recrui_wk_verified$total_verifieds)/cumsum(recrui_wk_verified$active_recruits))
-
 ########################################################################################################################################################
 ########################################################################################################################################################
 ########################################################################################################################################################
@@ -217,13 +211,12 @@ veri_act <- veri_act%>% mutate(
   biocol.week = ifelse(!is.na(biocol.time),ceiling(as.numeric(difftime(as_date(biocol.time+days(5)),as_date(recrstart.date), units="days"))/7),NA),
   bldcol.week = ifelse(!is.na(bldcol.time),ceiling(as.numeric(difftime(as_date(bldcol.time+days(5)),as_date(recrstart.date), units="days"))/7),NA))
 
-##to recreate the new variable of th competions based on the updated concept of completions as the completions 
-###of all four baseline modules and blood collections as Yes.
-veri_act <- veri_act%>% mutate(allacts0 = case_when(d_100767870 == 353358909 & biospeComplete==1 ~ 353358909,
-                                                    d_100767870 == 104430631  | biospeComplete %in% c(0,NA) ~ 104430631 ),
-                               allacts1 = case_when(d_100767870 == 353358909 & d_878865966 == 353358909 ~ 353358909, #surveys are completed and blood drawn
+#allacts1= all surveys are completed and blood has been collected
+veri_act <- veri_act%>% mutate(allacts1 = case_when(d_100767870 == 353358909 & d_878865966 == 353358909 ~ 353358909, #surveys are completed and blood drawn
                                                     d_100767870 == 104430631  | d_878865966 %in% c(0,NA) ~ 104430631 ))
 
+#time1 is the maximum time that a participant has reported, max(module.time, bldcol.time)
+#then converting this time into the running week counter
 veri_act$allacts.time1 <- apply(veri_act[,c("module.time","bldcol.time")],1,max)
 veri_act$allacts.week1 <- ceiling(as.numeric(difftime(as_date(as.POSIXct(veri_act$allacts.time1)+days(5)),
                                                       as_date(recrstart.date), units="days"))/7)
@@ -240,16 +233,17 @@ module.wk.total <- filter(veri_act,d_100767870==353358909) %>% arrange(module.we
   dplyr::summarize(total_module=n(),
                    module.time.max=max(as_date(module.time),rm.na=T))
 
+#individuals who have completed the blood draw
 bldcol.wk.total <- filter(veri_act,d_878865966 == 353358909) %>% arrange(bldcol.week,verified.week) %>% 
   group_by(bldcol.week)%>%
   dplyr::summarize(total_blood=n(),
                    bldcol.time.max=max(as_date(bldcol.time),rm.na=T))
 
+#merge together all 3 aggregated tables
 dt.merged1 <- merge(module.wk.total,bldcol.wk.total,by.x="module.week",by.y="bldcol.week",all.x=TRUE,all.y=TRUE)
 dt.merged1 <- merge(dt.merged1,allacts.wk.total,by.x="module.week",by.y="allacts.week1",all.x=TRUE,all.y=TRUE)  
-#veri_wk_svybld <- merge(verified.wk.total, dt.merged1,by.x="verified.week",by="module.week", all.x=TRUE,all.y=TRUE)
-
 recr_svybld_wk <-  merge(recrui_wk_verified,dt.merged1, by.x="recruit.week",by.y="module.week",all.x=TRUE,all.y=TRUE)
+
 
 vars <- c("active_recruits","active_verifieds","passive_verifieds","total_recruits","total_verifieds","total_module","total_blood","total_allacts")
 recr_svybld_wk <- recr_svybld_wk %>% mutate_at(vars,~replace_na(., 0)) %>%  arrange(recruit.week)%>% 
@@ -267,6 +261,7 @@ recr_svybld_wk <- recr_svybld_wk %>% mutate_at(vars,~replace_na(., 0)) %>%  arra
          total_veri_noact = total_verifieds - total_blood - total_module + total_allacts,
          total_veri_noact.cum=cumsum(total_veri_noact))
 
+#reshape the data from wide to long
 veri_svybld_wk <- melt(recr_svybld_wk[,c("recruit.week.date","total_anyact.cum","total_verified.cum", "total_blood.cum", "total_module.cum", "total_bldsvy.cum","total_veri_noact.cum")],#id.vars="Site", 
                        measure.vars=c("total_verified.cum", "total_anyact.cum","total_blood.cum", "total_module.cum", "total_bldsvy.cum","total_veri_noact.cum"), variable.name="Verified_type",
                        value.name="Verified_Activities_n") %>% 
@@ -278,15 +273,16 @@ veri_svybld_wk <- melt(recr_svybld_wk[,c("recruit.week.date","total_anyact.cum",
                                        Verified_type =="total_veri_noact.cum" ~ "Verified, no Activities")) %>%
   arrange(verified.activities)
 
+#convert the verified.activites variable to a factor variable
 veri_svybld_wk$verified.activities <- factor(veri_svybld_wk$verified.activities, 
                                              levels=c("Verified, no Activities","Blood + Surveys","Blood","Surveys","Surveys or Blood","Verified"))
 max_y <- max(veri_svybld_wk$Verified_Activities_n)
-breaks <- seq(1000,1000*round(max_y/1000,0),1000)
 
 
 # Extract unique monthly dates from data
 unique_monthly_dates <- unique(as.Date(format(veri_svybld_wk$recruit.week.date, "%Y-%m-01")))
 
+#plotly plot
 Fig_all.plotly <- plot_ly() %>%
   add_lines(data = veri_svybld_wk, x = ~as.Date(recruit.week.date), color = ~verified.activities,
             y = ~Verified_Activities_n) %>%
