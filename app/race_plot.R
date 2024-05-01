bfa4ddacffa5766ef79ff01dbe2644f4d0cb7f5b
rebexxxxxx <rebecca.sansale@gmail.com>
2024-05-01 15:42:34 -0400

    race plot acronyms

--- app/race_plot.R
+++ app/race_plot.R
@@ -49,3 +49,59 @@ fig <- fig %>% layout(title = c("Self-Reported Race of Participants Who Complete
   }
 }
 
+
+
+
+
+
+
+# Define a function to map full names to acronyms
+map_race_to_acronym <- function(race) {
+  switch(race,
+         "American Indian or Alaska Native" = "AIAN",
+         "Black, African American or African" = "BAAA",
+         "Hawaiian or other Pacific Islander" = "HPI",
+         "Middle Eastern or North African" = "MENA",
+         "Multi-race" = "Muti",
+         "Other" = "Other",
+         "Skipped this question" = "Skip",
+         "UNKNOWN" = "Unknown",
+         "White" = "W",
+         NULL
+  )
+}
+
+
+# Apply the function to create a new column with acronyms
+race_df$Acronym <- ifelse(race_df$race == "American Indian or Alaska Native", "AIAN",
+                   ifelse(race_df$race == "Asian", "A",
+                   ifelse(race_df$race == "Black, African American, or African", "BAAA",
+                   ifelse(race_df$race == "Hawaiian or other Pacific Islander", "HPI",
+                   ifelse(race_df$race == "Hispanic, Latino, or Spanish", "HLS",
+                   ifelse(race_df$race == "Middle Eastern or North African", "MENA",
+                   ifelse(race_df$race == "Multi-race", "Multi",
+                   ifelse(race_df$race == "Other", "Other",
+                   ifelse(race_df$race == "Skipped this question", "Skip",
+                   ifelse(race_df$race == "UNKNOWN", "Unknown",
+                    ifelse(race_df$race == "White", "W", NA)))))))))))
+                        
+
+# Create a Plotly pie chart
+fig <- plot_ly(race_df, labels = ~race, values = ~Count, type = 'pie',
+               text = race_df$Acronym,
+               textinfo = 'text',
+               marker = list(colors = 'blue'),
+               hoverinfo = 'label+percent',
+               insidetextorientation = 'radial',
+               domain = list(x = c(0.1, 0.9), y = c(0.1, 0.9)))
+
+# Customize the layout
+curr.date <- Sys.Date()
+fig <- fig %>% layout(title = c("Self-Reported Race of Participants Who Completed BOH Section"),
+                      xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
+                      yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
+                      font = list(family = "Noto Sans"),
+                      margin = list(t = 50))
+
+# Print the plot
+fig
