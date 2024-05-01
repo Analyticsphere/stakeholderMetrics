79862f6a4a1a1b3fbe4c65e3b88767600dfaef0d
rebexxxxxx <rebecca.sansale@gmail.com>
2024-05-01 15:45:08 -0400

    cleaned up code

--- app/race_plot.R
+++ app/race_plot.R
@@ -18,60 +18,6 @@ race_counts <- table(race_data$race)
 race_df <- as.data.frame(race_counts)
 names(race_df) <- c("race", "Count")
 
-#identify number of colors to use  
-unique_items <- unique(race_df$race)
-n_colors <- length(unique(race_df$race))
-
-# Ensure you have a sufficient number of colors for your activities
-cols <- select_colors(color_palette, n_colors)
-
-# Map colors to activities to ensure consistency
-color_mapping <- setNames(cols, unique_items)
-  
-# Create a Plotly pie chart
-fig <- plot_ly(race_df, labels = ~race, values = ~Count, type = 'pie',
-               textinfo='label',
-               marker = list(colors = color_mapping),
-               hoverinfo = 'label+percent',
-               insidetextorientation = 'radial',
-               domain = list(x = c(0.1, 0.9), y = c(0.1, 0.9)))
-  
-# Customize the layout
-curr.date <- Sys.Date()
-fig <- fig %>% layout(title = c("Self-Reported Race of Participants Who Completed BOH Section"),
-                      xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
-                      yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
-                      font = list(family = "Noto Sans"),
-                      margin = list(t = 50))
-  
-  # Print the plot
-  fig
-  }
-}
-
-
-
-
-
-
-
-# Define a function to map full names to acronyms
-map_race_to_acronym <- function(race) {
-  switch(race,
-         "American Indian or Alaska Native" = "AIAN",
-         "Black, African American or African" = "BAAA",
-         "Hawaiian or other Pacific Islander" = "HPI",
-         "Middle Eastern or North African" = "MENA",
-         "Multi-race" = "Muti",
-         "Other" = "Other",
-         "Skipped this question" = "Skip",
-         "UNKNOWN" = "Unknown",
-         "White" = "W",
-         NULL
-  )
-}
-
-
 # Apply the function to create a new column with acronyms
 race_df$Acronym <- ifelse(race_df$race == "American Indian or Alaska Native", "AIAN",
                    ifelse(race_df$race == "Asian", "A",
@@ -84,8 +30,17 @@ race_df$Acronym <- ifelse(race_df$race == "American Indian or Alaska Native", "A
                    ifelse(race_df$race == "Skipped this question", "Skip",
                    ifelse(race_df$race == "UNKNOWN", "Unknown",
                     ifelse(race_df$race == "White", "W", NA)))))))))))
-                        
 
+#identify number of colors to use  
+unique_items <- unique(race_df$race)
+n_colors <- length(unique(race_df$race))
+
+# Ensure you have a sufficient number of colors for your activities
+cols <- select_colors(color_palette, n_colors)
+
+# Map colors to activities to ensure consistency
+color_mapping <- setNames(cols, unique_items)
+  
 # Create a Plotly pie chart
 fig <- plot_ly(race_df, labels = ~race, values = ~Count, type = 'pie',
                text = race_df$Acronym,
@@ -105,3 +60,6 @@ fig <- fig %>% layout(title = c("Self-Reported Race of Participants Who Complete
 
 # Print the plot
 fig
+  }
+}
+
